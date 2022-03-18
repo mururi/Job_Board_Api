9b5dbf5ffab563c2992bbdf615c4d3b4a2c13c26
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-18 08:17:14 +0300

    Add company methods

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request, make_response
-from models import db, User
+from models import db, User, Company
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
+from flask_login import login_user, logout_user, login_required
 
 app = Flask(__name__)
 
@@ -14,11 +15,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-
-
-
-
-# db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 # @token_required decorator
 def token_required(f):
@@ -90,8 +88,8 @@ def get_all_users(current_user):
     return jsonify({'users': output})
 
 @app.route('/user/<public_id>', methods=['GET'])
-@token_required
-def get_one_user(current_user, public_id):
+#@token_required
+def get_one_user(public_id):
 
     user = User.query.filter_by(public_id = public_id).first()
 
@@ -159,6 +157,53 @@ def login():
         return jsonify({'token': token})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+
+# Company class endpoints
+@app.route('/company',methods = ["POST"])
+def register_company():
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_company = Company(
+        public_id=str(uuid.uuid4()),
+        name=data['name'],
+        email=data['email'],
+        password=hashed_password,
+        desc=data['description']
+    )
+
+    db.session.add(new_company)
+    db.session.commit()
+
+    return jsonify({'message': 'New company created successfully'}), 200
+
+@app.route('/company/login',methods=['GET','POST'])
+def company_login():
+    auth = request.authorization
+    
+    company = Company.query.filter_by(name=auth.username).first()
+
+    if not company:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if not company:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(company.password, auth.password):
+        token = jwt.encode({'public_id': company.public_id, 'exp': datetime.datetime.now() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token': token})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+@app.route('/company/logout')
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logged out successfully'}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -10,3 +10,12 @@ class User(db.Model):
     email = db.Column(db.String(128))
     password = db.Column(db.String(128))
     admin = db.Column(db.Boolean)
+
+class Company(db.Model):
+    __tablename__ = "companies"
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(128), unique=True)
+    name = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    desc = db.Column(db.String(255))
